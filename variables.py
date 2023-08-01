87178f81aacd631b04abbbde8fd3a743a893a9ec
MihailEC <mihail-ekimov@mail.ru>
2023-08-01 22:36:53 +0300

    New Lesson

---
+++ variables.py
@@ -0,0 +1,16 @@
+# Объект еда
+# Переменные
+
+name = 'Картофель фри'       # Наименование блюда
+fresh_deadline = 2           # Количество дней до истечения срока годности
+price_dish = 200             # Цена блюда
+tasty = True                 # Вкусное блюдо?
+fast = True                  # Быстрое приготовление?
+name_rest = 'Бургер Кинг'    # Наименование ресторана (кафе)
+
+print(type(name))            # Тип переменной name
+print(type(fresh_deadline))  # Тип переменной fresh_deadline
+print(type(price_dish))      # Тип переменной price_dish
+print(type(tasty))           # Тип переменной tasty
+print(type(fast))            # Тип переменной fast
+print(type(name_rest))       # Тип переменной name_rest
